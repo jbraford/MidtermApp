1a318a16cb4935f3409654431513755f849bb173
Julie Braford <jbraford@css.edu>
2020-03-22 15:52:46 -0500

    Initial commit

---
+++ app/src/main/java/com/example/midtermapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.midtermapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText tvName;
+    TextView tvResults;
+    Button btnPush;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvName = (EditText) findViewById(R.id.txtName);
+        tvResults = (TextView) findViewById(R.id.txtResults);
+        btnPush = (Button) findViewById(R.id.btnPush);
+
+
+    }
+    public void buttonPushClick (View view) {
+
+        tvResults.setText("is a great Android programmer!");
+
+
+
+    }
+}
